83f60bd1a2794bf6b82087bd23281395df8289f4
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-06-18 21:50:28 -0400

    updated AvengersList and Home

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 
+import Home from './components/Home';
+import AvengersList from './components/AvengersList';
+
+import avengers from './data';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      avengers: avengers
+    };
+  }
+
   render() {
     return (
       <div className='App'>
